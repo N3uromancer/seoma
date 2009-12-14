d776158568d75d675b396fbb9616cc0f44b4d036
kylehav <kylehav@db5dceac-bde4-11de-9a73-11f8458f6c97>
2009-12-14 03:11:04 +0000

    quick fix

--- G9/src/gameEngine/world/World.java
+++ G9/src/gameEngine/world/World.java
@@ -83,7 +83,7 @@ public class World
 		{
 			File f = new File(System.getProperty("user.dir")+System.getProperty("file.separator")+
 					System.getProperty("file.separator")+"maps"+System.getProperty("file.separator")+
-					"Standard.map");
+					"a test map with resources.map");
 			FileInputStream fis = new FileInputStream(f);
 			DataInputStream dis = new DataInputStream(fis);
 			m.readMap(dis);
