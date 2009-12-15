bd113be6e75147c4ecab362b36137ff7beb8cbda
aicommander <aicommander@db5dceac-bde4-11de-9a73-11f8458f6c97>
2009-12-15 02:43:00 +0000

    - Remove some of SimpleStarter and place it in a new Starter class  - Add a simple class loader

---
+++ trunk/G9/src/starter/SimpleClassLoader.java
@@ -0,0 +1,34 @@
+package starter;
+
+import gameEngine.world.owner.Owner;
+import gameEngine.world.unit.Unit;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class SimpleClassLoader extends ClassLoader {	
+	public Class loadClassFromFile(File f) throws IOException
+	{
+		byte[] fileData;
+		FileInputStream fIn = new FileInputStream(f);
+		String className = f.getName().substring(0, f.getName().lastIndexOf(".")-1);
+		
+		/* FIXME: We probably shouldn't be using available() like this */
+		fileData = new byte[fIn.available()];
+		
+		fIn.read(fileData);
+		
+		System.out.println("ClassLoader: Loading "+className);
+		return defineClass(className, fileData, 0, fileData.length);
+	}
+	
+	public Object constructObjectFromClass(Class c, Class[] argTypes, Object[] args) throws IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException, SecurityException, NoSuchMethodException
+	{
+		Constructor<?> con = c.getConstructor(argTypes);
+		return con.newInstance(args);
+	}
+}
--- trunk/G9/src/starter/SimpleStarter.java
+++ trunk/G9/src/starter/SimpleStarter.java
@@ -28,36 +28,12 @@ public class SimpleStarter
 		String[] startingUnits = {"leader"};
 		//String[] startingUnits = {"factory s1", "builder s1"};
 		StartSettings ss = new StartSettings(700, 700, owners, startingUnits);
-		
-		
 		//StartSettings ss = new StartSettings(500, 500, new Owner[]{new Owner("player 1", c1)}, startingUnits);
 
 		HashMap<Owner, AI> ais = new HashMap<Owner, AI>();
 		ais.put(owners[0], new RapestroyAI(owners[0]));
 		ais.put(owners[1], new RapestroyAI(owners[1]));
-		final World w = new World(ss, ais);
 		
-		GLCanvas c = new GLCanvas(new GLCapabilities());;
-		final GameEngine ge = new GameEngine(false, c)
-		{
-			public void updateGame(double tdiff, HashMap<Byte, ArrayList<UserInput>> ui)
-			{
-				w.updateWorld(tdiff, ui);
-			}
-		};
-		DisplayManager dm = new DisplayManager()
-		{
-			public void display(GL gl, int width, int height)
-			{
-				w.drawWorld(owners[0], width, height, gl);
-			}
-		};
-		UIFrame f = new UIFrame(ge, c, dm);
-		f.addWindowListener(new WindowAdapter(){
-			public void windowClosing(WindowEvent e)
-			{
-				System.exit(0);
-			}
-		});
+		Starter.startGame(ais, ss, owners);
 	}
 }
