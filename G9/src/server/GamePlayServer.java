a1e601158be6e6f021330efcebafcb7576d6245b
aicommander <aicommander@db5dceac-bde4-11de-9a73-11f8458f6c97>
2009-12-15 04:18:52 +0000

    - Fix several bugs in the class loader and server  - The server works now! :D

--- G9/src/server/GamePlayServer.java
+++ G9/src/server/GamePlayServer.java
@@ -21,55 +21,18 @@ public class GamePlayServer extends Thread implements HookCallbacks {
 	HookServ serv;
 	SimpleClassLoader cLoader;
 	ArrayList<AIListEntry> ais;
+	GamePlayThread currThread;
 	
 	public GamePlayServer() {
 		System.out.print("GamePlayServer: starting...");
 		serv = new HookServ("gutman.dyndns.org", 10, "AI-SERVER", "SERVER-ACCT", this);
 		cLoader = new SimpleClassLoader();
 		ais = new ArrayList<AIListEntry>();
+		currThread = null;
 		System.out.println("done");
 		System.out.println("Waiting for files...");
 	}
-	
-	public void run() {
-			for (AIListEntry aiEntry1 : ais)
-			{
-				for (AIListEntry aiEntry2 : ais)
-				{
-					if (aiEntry1 != aiEntry2 && !aiEntry1.losses.contains(aiEntry2.c) &&
-						!aiEntry1.wins.contains(aiEntry2.c))
-					{
-						/* Shamefully copied from SimpleStarter */
-						double[] c1 = {1, 0, 0};
-						double[] c2 = {0, 0, 1};
-						final Owner[] owners = {new Owner((byte)0, "player 1", c1), new Owner((byte)2, "player 2", c2)};
-						String[] startingUnits = {"leader"};
-						//String[] startingUnits = {"factory s1", "builder s1"};
-						StartSettings ss = new StartSettings(700, 700, owners, startingUnits);
-						//StartSettings ss = new StartSettings(500, 500, new Owner[]{new Owner("player 1", c1)}, startingUnits);
 
-						HashMap<Owner, AI> aiHashMap = new HashMap<Owner, AI>();
-						try {
-							aiHashMap.put(owners[0], (AI) cLoader.constructObjectFromClass(aiEntry1.c, new Class[] {Owner.class}, new Object[] {owners[0]}));
-							aiHashMap.put(owners[1], (AI) cLoader.constructObjectFromClass(aiEntry2.c, new Class[] {Owner.class}, new Object[] {owners[1]}));
-						} catch (Exception e) {
-							e.printStackTrace();
-							return;
-						}
-						
-						/* FIXME: We don't track stats properly yet */
-						aiEntry1.wins.add(aiEntry2.c);
-						aiEntry2.wins.add(aiEntry1.c);
-						
-						System.out.println("Playing "+aiEntry1.c.getName()+" vs. "+aiEntry2.c.getName());
-						Starter.startGame(aiHashMap, ss, owners);
-						
-						/* TODO: Record results */
-					}
-				}
-			}
-		}
-	
 	public static void main(String[] args) {
 		new GamePlayServer();
 	}
@@ -104,11 +67,65 @@ public class GamePlayServer extends Thread implements HookCallbacks {
 		System.out.println("Loaded class from: "+f.getName());
 		
 		/* Wait for the worker to die so we don't get a concurrent modification exception */
-		while (this.isAlive()) Thread.yield();
+		if (currThread != null)
+			while (currThread.t.isAlive()) Thread.yield();
 		
 		ais.add(new AIListEntry(c));
 		
-		start();
+		currThread = new GamePlayThread(cLoader, ais);
+	}
+}
+
+class GamePlayThread implements Runnable {
+	ArrayList<AIListEntry> ais;
+	SimpleClassLoader cLoader;
+	Thread t;
+	
+	public GamePlayThread(SimpleClassLoader cLoader, ArrayList<AIListEntry> ais)
+	{
+		this.ais = ais;
+		this.cLoader = cLoader;
+		t = new Thread(this);
+		t.start();
+	}
+	
+	public void run() {
+		for (AIListEntry aiEntry1 : ais)
+		{
+			for (AIListEntry aiEntry2 : ais)
+			{
+				if (aiEntry1 != aiEntry2 && !aiEntry1.losses.contains(aiEntry2.c) &&
+					!aiEntry1.wins.contains(aiEntry2.c))
+				{
+					/* Shamefully copied from SimpleStarter */
+					double[] c1 = {1, 0, 0};
+					double[] c2 = {0, 0, 1};
+					final Owner[] owners = {new Owner((byte)0, "player 1", c1), new Owner((byte)2, "player 2", c2)};
+					String[] startingUnits = {"leader"};
+					//String[] startingUnits = {"factory s1", "builder s1"};
+					StartSettings ss = new StartSettings(700, 700, owners, startingUnits);
+					//StartSettings ss = new StartSettings(500, 500, new Owner[]{new Owner("player 1", c1)}, startingUnits);
+
+					HashMap<Owner, AI> aiHashMap = new HashMap<Owner, AI>();
+					try {
+						aiHashMap.put(owners[0], (AI) cLoader.constructObjectFromClass(aiEntry1.c, new Class[] {Owner.class}, new Object[] {owners[0]}));
+						aiHashMap.put(owners[1], (AI) cLoader.constructObjectFromClass(aiEntry2.c, new Class[] {Owner.class}, new Object[] {owners[1]}));
+					} catch (Exception e) {
+						e.printStackTrace();
+						return;
+					}
+					
+					/* FIXME: We don't track stats properly yet */
+					aiEntry1.wins.add(aiEntry2.c);
+					aiEntry2.wins.add(aiEntry1.c);
+					
+					System.out.println("Playing "+aiEntry1.c.getName()+" vs. "+aiEntry2.c.getName());
+					Starter.startGame(aiHashMap, ss, owners);
+					
+					/* TODO: Record results */
+				}
+			}
+		}
 	}
 }
 
