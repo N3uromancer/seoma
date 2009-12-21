9bc6907d819418176c54807fc19ce84e37116158
kylehav <kylehav@db5dceac-bde4-11de-9a73-11f8458f6c97>
2009-12-21 02:24:58 +0000

    Updated my AI, made a new 1v1 map, reduced range of DefenseTurret.

--- G9/src/gameEngine/world/World.java
+++ G9/src/gameEngine/world/World.java
@@ -101,7 +101,7 @@ public class World
 		{
 			File f = new File(System.getProperty("user.dir")+System.getProperty("file.separator")+
 					System.getProperty("file.separator")+"maps"+System.getProperty("file.separator")+
-					"cross 4 players.map");
+					"1v1.map");
 			FileInputStream fis = new FileInputStream(f);
 			DataInputStream dis = new DataInputStream(fis);
 			m.readMap(dis);
