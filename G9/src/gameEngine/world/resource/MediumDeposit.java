260b04d1db713bd819125bcd9a9b093697e4aa25
aicommander <aicommander@db5dceac-bde4-11de-9a73-11f8458f6c97>
2010-01-31 21:06:17 +0000

    - Kill the old G9 code

--- G9/src/gameEngine/world/resource/MediumDeposit.java
+++
@@ -1,9 +0,0 @@
-package gameEngine.world.resource;
-
-public class MediumDeposit extends ResourceDeposit
-{
-	public MediumDeposit(double[] location)
-	{
-		super(location, 1500, 15, 70, 11);
-	}
-}
