1b23fef5d4c249f71cc459ea902e3d2820bba51c
kylehav <kylehav@db5dceac-bde4-11de-9a73-11f8458f6c97>
2010-07-26 03:47:01 +0000

    Made melee attacks.

--- the super rpg/src/world/attack/MeleeAttack.java
+++ the super rpg/src/world/attack/MeleeAttack.java
@@ -1,28 +1,40 @@
 package world.attack;
 
-import geom.Rectangle;
-
+import java.awt.Color;
 import java.awt.Graphics2D;
+import java.util.HashSet;
+import java.util.Iterator;
+
+import geom.Rectangle;
 
 import world.World;
 import world.item.weapon.Weapon;
 import world.modifier.Drawable;
 import world.networkUpdateable.NetworkUpdateable;
 import world.unit.Unit;
+import world.unit.attribute.Attribute;
 
 public class MeleeAttack extends NetworkUpdateable implements Drawable
 {
-	private Unit u;
+	private Unit ownerUnit;
 	private Weapon w;
 	private byte direction;
+	private double duration;
+	private double x;
+	private double y;
+	private short id;
 	
 	public MeleeAttack(boolean isGhost, short id, Unit u, Weapon w, byte direction)
 	{
 		super(isGhost, id, 0, false);
 		
-		this.u = u;
+		this.ownerUnit = u;
 		this.w = w;
 		this.direction = direction;
+		this.duration = 1.0;
+		this.x = u.getLocation()[0];
+		this.y = u.getLocation()[1];
+		this.id = id;
 		setReady();
 	}
 	public byte[] getState()
@@ -35,27 +47,43 @@ public class MeleeAttack extends NetworkUpdateable implements Drawable
 	}
 	public void simulate(World w, double tdiff)
 	{
-		//swing sword here
+		duration -= tdiff;
+		if (duration <= 0.0)
+			setDead();
 	}
 	public void update(World w, double tdiff)
 	{
-		//swing sword here
+		simulate(w, tdiff);
 		//damage hit units here
-	}
-	public boolean isRelevant(short id, World w)
-	{
-		return false;
+		HashSet hitUnits = new HashSet<Unit>();
+		hitUnits = w.getAssociatedRegion(id).getIntersectedUnits(this);
+		Iterator<Unit> i = hitUnits.iterator();
+		while(i.hasNext())
+		{
+			Unit u = i.next();
+			if(!u.equals(ownerUnit))
+				u.getAttributeManager().setAttribute(Attribute.health, 0);
+		}
 	}
 	public void draw(Graphics2D g)
 	{
-		
+		g.setColor(Color.WHITE);
+		g.fillRect((int)x-40, (int)y-40, 80, 80);
 	}
 	public boolean isDisplayed()
 	{
+		if (duration <= 0.0)
+			return false;
 		return true;
+	}
+	@Override
+	public boolean isRelevant(short id, World w) 
+	{
+		// TODO Auto-generated method stub
+		return false;
 	}
 	public Rectangle getBounds()
 	{
-		return null;
+		return new Rectangle(x-40, y-40, 80, 80);
 	}
 }
